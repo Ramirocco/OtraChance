b5a450b5f359a1aa1d3bc1dcf6aad6134cb8f01d
Ramirocco <105560507+Ramirocco@users.noreply.github.com>
2023-01-19 00:16:52 -0300

    agregue boton e input inicial

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+import { Button } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Escriba producto' style={styles.inputAddItem} />
+        <Button icon="playlist-check" mode="outlined" style={styles.ButtonAddItem} onPress={(e) =>
+          //agregar el valor de e a un array
+        {} }>Agregar
+        </Button>
+      </View >
+      <View style={styles.listContainer}>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    paddingTop: 10
   },
+
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  inputAddItem: {
+    width: 200,
+    borderBottomColor: "black",
+    borderBottomWidth: "1"
+  },
+
+  ButtonAddItem: {
+    marginLeft: 20,
+    color: "yelow"
+  },
+  listContainer:{ flex:3}
 });
